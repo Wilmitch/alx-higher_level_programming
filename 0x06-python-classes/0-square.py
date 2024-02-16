6553f93c0a6fbb3ec5510f9c3870b22a0fe74e19
wilmitch <mwangimwilfred@gmail.com>
2024-02-16 22:53:25 +0300

    correction on 0-square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Defines a class Square"""
+
 
 class Square:
-    """Represent a square."""
+
+    """Declears an empty class Square"""
     pass
