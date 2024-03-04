6979ce3e3947bfe7b91e8524ee06626772c4c36a
wilmitch <mwangimwilfred@gmail.com>
2024-03-04 16:32:44 +0300

    corrections to 4-inherits from file and 2-same class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
-"""Defines an inherited class-checking function."""
+"""This defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
-    
+    """This checks if an object is an inherited instance of a class.
     Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
+        obj (any): This is the object to check.
+        a_class (type): This is the class to match the type of obj to.
     Returns:
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
