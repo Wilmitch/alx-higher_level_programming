f9061e2f2ff841ac618c1cc5f339057583b42dd5
wilmitch <mwangimwilfred@gmail.com>
2024-05-13 21:35:33 +0300

    string methods

---
+++ 1-python_practice/15-python_string_methods.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+course = "Python for Beginners"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('o'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print('Python' in course)
+#find method returns the index of the character/s we are looking for
+#the in method returns a boolean value, True or False depending with if the character/s are there or not
+
