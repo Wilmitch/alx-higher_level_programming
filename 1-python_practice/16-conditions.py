497dd2c77728ec9b05015a005657fa7d22f1bab3
wilmitch <mwangimwilfred@gmail.com>
2024-05-17 16:48:20 +0300

    conditions in python

---
+++ 1-python_practice/16-conditions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a Hot day")
+    print("Drink plenty of water!")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+    print("Enjoy your day")
